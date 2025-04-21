e0baaa116a548caa5d7f089d2f7c502df74f7392
sakibkhan <sakibkhan.stud@gmail.com>
2025-04-21 10:07:24 +0600

    added log in page, redirected the statics into static folder

--- capstoneProject/BTdetection/urls.py
+++ capstoneProject/BTdetection/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
-    path('', include('members.urls')),
-    path('admin/', admin.site.urls),
+    path('hello/', views.hello , name='hello'),
+    path('login/', views.login , name='login'),
 ]
--- capstoneProject/BTdetection/views.py
+++ capstoneProject/BTdetection/views.py
@@ -1,2 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+def login(request):
+    #Rendering the log in page
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
+
+def hello(request):
+    #Rendering the hello page
+    return HttpResponse("Hello, world!")
